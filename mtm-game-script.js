1385e1bd65d32fafea1d982d328878035f19c22e
JosePaulo95 <fernandes.josepaulo95@gmail.com>
2018-10-31 17:04:43 -0300

    iniciando c8 joguinho

---
+++ mtm-game-script.js
@@ -0,0 +1,25 @@
+
+
+
+var secao_ativa = 1;
+
+function respondeu(){
+	var score = getUrlParam('score', 0);
+	score ++;
+	window.location.replace("c8-pergunta.html?score="+score);
+}
+
+function getUrlParam(parameter, defaultvalue){
+    var urlparameter = defaultvalue;
+    if(window.location.href.indexOf(parameter) > -1){
+        urlparameter = getUrlVars()[parameter];
+        }
+    return urlparameter;
+}
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
